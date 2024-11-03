ab8567a2c65346153e30012c3cb182b75bad35c3
Dvis <gokhaledurgesh4@gmail.com>
2024-11-03 11:32:46 +0530

    Auto Fixes

--- ERAVIBES/core/userbot.py
+++ ERAVIBES/core/userbot.py
@@ -65,10 +65,10 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("+7ehnJA3aMb84OGNl")
-                await self.one.join_chat("+7ehnJA3aMb84OGNl")
-                await self.one.join_chat("+7ehnJA3aMb84OGNl")
-                await self.one.join_chat("+7ehnJA3aMb84OGNl")
+                await self.one.join_chat("ai_image_junction")
+                await self.one.join_chat("ai_image_junction")
+                await self.one.join_chat("ai_image_junction")
+                await self.one.join_chat("ai_image_junction")
             except:
                 pass
             assistants.append(1)
@@ -93,10 +93,10 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("+7ehnJA3aMb84OGNl")
-                await self.two.join_chat("+7ehnJA3aMb84OGNl")
-                await self.two.join_chat("+7ehnJA3aMb84OGNl")
-                await self.two.join_chat("+7ehnJA3aMb84OGNl")
+                await self.two.join_chat("ai_image_junction")
+                await self.two.join_chat("ai_image_junction")
+                await self.two.join_chat("ai_image_junction")
+                await self.two.join_chat("ai_image_junction")
             except:
                 pass
             assistants.append(2)
@@ -121,10 +121,10 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("+7ehnJA3aMb84OGNl")
-                await self.three.join_chat("+7ehnJA3aMb84OGNl")
-                await self.three.join_chat("+7ehnJA3aMb84OGNl")
-                await self.three.join_chat("+7ehnJA3aMb84OGNl")
+                await self.three.join_chat("ai_image_junction")
+                await self.three.join_chat("ai_image_junction")
+                await self.three.join_chat("ai_image_junction")
+                await self.three.join_chat("ai_image_junction")
             except:
                 pass
             assistants.append(3)
@@ -149,10 +149,10 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("+7ehnJA3aMb84OGNl")
-                await self.four.join_chat("+7ehnJA3aMb84OGNl")
-                await self.four.join_chat("+7ehnJA3aMb84OGNl")
-                await self.four.join_chat("+7ehnJA3aMb84OGNl")
+                await self.four.join_chat("ai_image_junction")
+                await self.four.join_chat("ai_image_junction")
+                await self.four.join_chat("ai_image_junction")
+                await self.four.join_chat("ai_image_junction")
             except:
                 pass
             assistants.append(4)
@@ -177,10 +177,10 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("+7ehnJA3aMb84OGNl")
-                await self.five.join_chat("+7ehnJA3aMb84OGNl")
-                await self.five.join_chat("+7ehnJA3aMb84OGNl")
-                await self.five.join_chat("+7ehnJA3aMb84OGNl")
+                await self.five.join_chat("ai_image_junction")
+                await self.five.join_chat("ai_image_junction")
+                await self.five.join_chat("ai_image_junction")
+                await self.five.join_chat("ai_image_junction")
             except:
                 pass
             assistants.append(5)
