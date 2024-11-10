7126922105d1b0257c48e061492c246498b5759b
Dvis <gokhaledurgesh4@gmail.com>
2024-11-10 17:11:09 +0530

    Update memorydatabase.py

--- ERAVIBES/utils/database/memorydatabase.py
+++ ERAVIBES/utils/database/memorydatabase.py
@@ -1,19 +1,11 @@
-#
-# Copyright (C) 2024 by IamDvis@Github, < https://github.com/IamDvis >.
-#
-# This file is part of < https://github.com/IamDvis/DV-MUSIC > project,
-# and is released under the MIT License.
-# Please see < https://github.com/IamDvis/DV-MUSIC/blob/master/LICENSE >
-#
-# All rights reserved.
-#
+
 
 import json
 import os
 from typing import Dict, List, Union
 
 import config
-from ERAVIBES.core.mongo import mongodb
+from CHUHAMUSIC.core.mongo import mongodb
 
 channeldb = mongodb.cplaymode
 commanddb = mongodb.commands
