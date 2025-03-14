0c79688528d6ff03995ca2c11a3dfb7ba244ea7c
TEAM ISTKHAR <mdistkhar1078@gmail.com>
2025-03-14 11:00:28 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -54,7 +54,7 @@ EXTRA_PLUGINS = getenv(
 
 EXTRA_PLUGINS_REPO = getenv(
     "EXTRA_PLUGINS_REPO",
-    "https://github.com/IamDvis/DV-EXTRAPLUGIN",
+    "https://github.com/istkharalam62//TEAM_-ISTKHAR-EXTRAPLUGIN",
 )
 # Fill here the external plugins repo where plugins that you want to load
 
@@ -97,7 +97,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/IamDvis/DV-MUSIC",
+    "https://github.com/istkharalam62/CREW-MUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
@@ -118,10 +118,10 @@ AUTO_GCAST_MSG = getenv("AUTO_GCAST_MSG", "")
 
 
 SUPPORT_CHANNEL = getenv(
-    "SUPPORT_CHANNEL", "https://t.me/ai_image_junction"
+    "SUPPORT_CHANNEL", "https://t.me/THUNDERDEVS"
 )  # Example:- https://t.me/ai_image_junction
 SUPPORT_GROUP = getenv(
-    "SUPPORT_GROUP", "https://t.me/+7ehnJA3aMb84OGNl"
+    "SUPPORT_GROUP", "https://t.me/+pnDJxCG5VVphMTVl"
 )  # Example:- https://t.me/+7ehnJA3aMb84OGNl
 
 SUPPORT_CHAT = getenv(
@@ -129,7 +129,7 @@ SUPPORT_CHAT = getenv(
 )  # Example:- https://t.me/+7ehnJA3aMb84OGNl
 
 # Set it in True if you want to leave your assistant after a certain amount of time. [Set time via AUTO_LEAVE_ASSISTANT_TIME]
-AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "True")
+AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "false")
 
 # Time after which you're assistant account will leave chats automatically.
 AUTO_LEAVE_ASSISTANT_TIME = int(
@@ -149,7 +149,7 @@ TELEGRAM_DOWNLOAD_EDIT_SLEEP = int(getenv("TELEGRAM_EDIT_SLEEP", "5"))
 
 
 # Your Github Repo.. Will be shown on /start Command
-GITHUB_REPO = getenv("GITHUB_REPO", "https://github.com/IamDvis/DV-MUSIC")
+GITHUB_REPO = getenv("GITHUB_REPO", "https://github.com/istkharalam62/CREW-MUSIC")
 
 
 # Spotify Client.. Get it from https://developer.spotify.com/dashboard
