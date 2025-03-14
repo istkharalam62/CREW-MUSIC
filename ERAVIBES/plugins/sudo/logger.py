3990ea2382d3fcbf7afff3e7b99d19276e64fb82
TEAM ISTKHAR <mdistkhar1078@gmail.com>
2025-03-14 11:55:14 +0530

    Update logger.py

--- ERAVIBES/plugins/sudo/logger.py
+++ ERAVIBES/plugins/sudo/logger.py
@@ -3,7 +3,7 @@ from pyrogram import filters
 from ERAVIBES import app
 from ERAVIBES.misc import SUDOERS
 from ERAVIBES.utils.database import add_off, add_on
-from SONALI.utils.decorators.language import language
+from ERAVIBES.utils.decorators.language import language
 
 
 @app.on_message(filters.command(["logger"]) & SUDOERS)
